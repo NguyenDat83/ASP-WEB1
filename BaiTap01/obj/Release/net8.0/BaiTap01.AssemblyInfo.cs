3a97674980b17f5a719f703d6404d5937dd8442c
NguyenDat83 <dat334142@gmail.com>
2024-08-27 19:44:11 +0700

    Hoan Thanh BaiTap02

--- BaiTap01/obj/Release/net8.0/BaiTap01.AssemblyInfo.cs
+++ BaiTap01/obj/Release/net8.0/BaiTap01.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap01")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea65a5dbe7cd0c434c9dfa55f227fcc838160d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap01")]
