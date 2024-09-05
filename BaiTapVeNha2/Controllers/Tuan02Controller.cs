cf80952c2aaf5c4788b9b81637872cbd1d8c2f99
NguyenDat83 <dat334142@gmail.com>
2024-09-05 22:08:26 +0700

    Hoan Thanh Bai tap ve nha

---
+++ BaiTapVeNha2/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha2.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nguyễn Quốc Đạt";
+            ViewBag.MSSV = "1822041217";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+    }
+}
